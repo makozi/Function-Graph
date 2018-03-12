42c663b6c7da7a1f2c2eb7b2fe6ebbc7e4cd384a
MARIZU MAKOZI <marizumakozi97@gmail.com>
2018-03-12 17:25:53 +0100

    Add files via upload

---
+++ function.js
@@ -0,0 +1,10 @@
+functionPlot({
+    target:'#myFunction',
+    data:[{
+        fn:'sin(x)',
+        color:'red'
+    }],
+    grid:true,
+    yAxis:{domain: [-1,1]},
+    xAxis:{domain:[0,2*Math.PI]},
+});
